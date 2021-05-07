515d864c1c61faba09cc7bc6f43b72f00904abe5
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-05-07 23:20:30 +0100

    Implement window_manager.cpp/h

---
+++ src/window_manager.cpp
@@ -0,0 +1,25 @@
+#include "window_manager.h"
+
+void WindowManager::draw_all ()
+{
+  window.draw (resource_manager.get_background_sprite ());
+  window.draw (resource_manager.get_player_sprite ());
+  window.display ();
+}
+
+void WindowManager::start_window ()
+{
+  resource_manager.initialise_window ();
+  while (window.isOpen ())
+  {
+	sf::Event e{};
+	while (window.pollEvent (e))
+	{
+	  if (e.type == sf::Event::Closed)
+	  {
+		window.close ();
+	  }
+	}
+	draw_all ();
+  }
+}
---
+++ src/window_manager.h
@@ -0,0 +1,26 @@
+#ifndef _WINDOW_MANAGER_H_
+#define _WINDOW_MANAGER_H_
+
+#include "SFML/Graphics.hpp"
+#include "resource_manager.h"
+
+class WindowManager {
+
+ public:
+  /**
+   * Draws all managed resources to the window, ready to be painted by the next
+   * call to display.
+   */
+  void draw_all ();
+  /**
+   * Starts the main window and the game loop.
+   */
+  void start_window ();
+
+ private:
+  ResourceManager resource_manager;
+  sf::RenderWindow window{sf::VideoMode (1600, 900, 32), "Fantasia",
+						  sf::Style::Close};
+};
+
+#endif//_WINDOW_MANAGER_H_
