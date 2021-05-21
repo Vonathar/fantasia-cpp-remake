3fec9e553d5b36efa8ae63cce5755c488e80a070
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-05-21 19:41:29 +0100

    Dynamically calculate coordinates of enemy texture.

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -2,9 +2,11 @@
 
 Enemy::Enemy(Resources &r) : resources(r), gui(r)
 {
-  sprite.setTexture(resources.get_enemy_texture(EnemyName::FAIRY_FILIA));
-  sprite.setPosition(1040.0f, 650.0f);
-  sprite.setScale(0.9f, 0.9f);
+  sf::Texture &texture = resources.get_enemy_texture(EnemyName::FAIRY_FILIA);
+  sprite.setTexture(texture);
+  sprite.setPosition(1095.0f - (static_cast<float>(texture.getSize().x) / 2.0f),
+                     740.0f - (static_cast<float>(texture.getSize().y) / 2.0f));
+  sprite.setScale(0.8f, 0.8f);
 }
 
 void Enemy::render(sf::RenderTarget &target)
