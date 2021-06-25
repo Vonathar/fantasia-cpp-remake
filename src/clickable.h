25b2f538a51723966c418f59ac39323e2964689a
Gianmarco Caputo <gianmarco.caputo.uk@gmail.com>
2021-06-25 22:02:41 +0100

    Add clickable.h interface.

---
+++ src/clickable.h
@@ -0,0 +1,13 @@
+#ifndef _CLICKABLE_H_
+#define _CLICKABLE_H_
+
+#include <SFML/Graphics.hpp>
+
+class Clickable
+{
+  public:
+  virtual bool boundaries_contain(sf::Vector2f &pos) = 0;
+  virtual void handle_click() = 0;
+};
+
+#endif // _CLICKABLE_H_
